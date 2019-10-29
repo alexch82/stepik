cf6d35c0dd21f6e5586732d9a0749a389f77be13
alexey.chernous <alexch82@ya.ru>
2019-10-29 20:08:28 +0300

    Add copyright

--- fr2.cpp
+++ fr2.cpp
@@ -1,3 +1,11 @@
+/*
+ * Решение задач курса "Введение в программирование С++"
+ * 
+ * [URL=https://stepik.org/course/363/promo]
+ * 
+ * Copyright 2019 Черноус Алексей <alexch82@ya.ru>
+ * 
+ */
 #include <iostream>
 #include <cmath>
 
