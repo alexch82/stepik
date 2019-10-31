d77bf4bc05838c2cf9aaa9052c9a665c48b002aa
Chernous Alex <alexch82@ya.ru>
2019-10-31 13:50:34 +0300

    New task in python

--- python/task1.12-7.py
+++ python/task1.12-7.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
-n = int(input())
-p = 'программист'
-if (n % 10 == 0) or (5 <= n % 10 and n % 10 <= 9) or (11 <= n % 100 and n % 100 <= 19):
-	print(n, p+'ов')
-elif 2 <= n % 10 and n % 10 <= 4:
-	print(n, p+'а')
+s = str(input())
+if (int(s[0]) + int(s[1]) + int(s[2])) == (int(s[3]) + int(s[4]) + int(s[5])):
+	print('Счастливый')
 else:
-	print(n, p)
+	print('Обычный')
