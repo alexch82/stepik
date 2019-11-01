8540c5f1225235909aec1f327fcf31de12d90b66
Chernous Alex <alexch82@ya.ru>
2019-11-01 23:12:02 +0300

    Add new files

--- cpp/if14.cpp
+++ cpp/if14.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 
 int main() {
-  // put your code here
-    int a1, b1, c1, a2, b2, c2;
-    const char* equal = "Boxes are equal";
-    const char* small = "The first box is smaller than the second one,";
-    const char* large = "The first box is larger than the second one";
-    std::cin >> a1 >> b1 >> c1 >> a2 >> b2 >> c2;
-    if (a1 * b1 * c1 < a2 * b2 * c2) std::cout << small;
-    else if (a1 * b1 * c1 == a2 * b2 * c2) std::cout << equal;
-    else std::cout << large;
-  return 0;
+	int a1, b1, c1, a2, b2, c2;
+	const char* equal = "Boxes are equal";
+	const char* small = "The first box is smaller than the second one,";
+	const char* large = "The first box is larger than the second one";
+	std::cin >> a1 >> b1 >> c1 >> a2 >> b2 >> c2;
+	if (a1 * b1 * c1 < a2 * b2 * c2) std::cout << small;
+	else if (a1 * b1 * c1 == a2 * b2 * c2) std::cout << equal;
+	else std::cout << large;
+	return 0;
 }
