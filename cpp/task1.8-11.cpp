2ef76d3dd2e852bd80e1489f04c0ef446aaa0457
Черноус Алексей <alexch82@ya.ru>
2019-11-25 23:55:20 +0300

    Change task

--- cpp/task1.8-11.cpp
+++ cpp/task1.8-11.cpp
@@ -14,16 +14,20 @@ int main()
 	int a[100][100] = {};
 	int n, m;
 	std::cin >> n >> m;
-	if (n >= m) {
-		for (int i = 0; i < n; i++)
-			for (int j = 0; j < m; j++) {
-				std::cout << std::setw(4);
-			}
-	} else {
-		for (int i = 0; i < n; i++)
-			for (int j = 0; j < m; j++) {
-				std::cout << std::setw(4);
-			}
+	int i = 0, j = 0, k = 1, di = 0, dj = -1;
+	while (k <= n * m) {
+		a[i][j] = k;
+		if (i)
+		i += di;
+		j += dj;
+		k++;
+	}
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < m; j++) {
+			std::cout << std::setw(4);
+			std::cout << a[i][j];
+		}
+		std::cout << std::endl;
 	}
 	return 0;
 }
