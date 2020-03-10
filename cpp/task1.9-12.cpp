db606113c50938e410808474ed0dfc4df1229f0d
Черноус Алексей <alexch82@ya.ru>
2020-03-10 10:39:18 +0300

    Change task

--- cpp/task1.9-12.cpp
+++ cpp/task1.9-12.cpp
@@ -3,7 +3,7 @@
  * 
  * [URL=https://stepik.org/course/363/promo]
  * 
- * Copyright 2019 Черноус Алексей <alexch82@ya.ru>
+ * Copyright 2020 Черноус Алексей <alexch82@ya.ru>
  * 
  */
 #include <iostream>
